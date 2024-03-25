7140b8237e228f6f45e2ffed4001cc0c835cfe20
Jeremy <jeremy.gaudin@ynov.com>
2024-03-25 10:08:55 +0100

    added class Object

---
+++ object.ts
@@ -0,0 +1,41 @@
+export class Object {
+    name : string;
+    hpGain : number;
+    resurectGain : number;
+    manaGain : number;
+
+    constructor(name : string, hpGain : number, resurectGain : number, manaGain : number) {
+        this.name = name
+        this.hpGain = hpGain
+        this.resurectGain = resurectGain
+        this.manaGain = manaGain
+    }
+
+    heal(charcter : Character) {
+        if (this.name == "Ether" ) {
+            console.log("Ether ne peut pas utiliser la fonction Heal!")
+        } else {
+            character.heal(this.hpGain)
+        }
+    }
+
+    resurect(charater : Character) {
+        if (this.name == "Ether") {
+            console.log("Ether ne peut pas utiliser la fonction Heal!")
+        } else {
+            if (character.currentHp < 0) {
+                character.heal(this.hpGain)
+            } else {
+                character.resurect(this.resurectGain)
+            }
+        }
+    }
+
+    gainMana(mage : Mage) {
+        if (this.name != "Ether") {
+            console.log("Only Ether can use gainMana")
+        } else {
+            mage.gainMana(this.manaGain)
+        }
+    }
+}
