bab46b80582b3c15f986ca023325d50d940a4b87
cmatthie <matthieu.caron@ynov.com>
2024-03-27 11:31:25 +0100

    Added interfaces (merge)

--- character/character.ts
+++ character/character.ts
@@ -8,7 +8,7 @@ export default class Character {
     className : string;
 
     constructor(name : string, attack : number, defense : number,speed : number, maxHP : number, currentHP : number, className : string){
-        this.name = name
+        this.name = name;
         this.attack = attack;
         this.defense = defense;
         this.speed = speed;
@@ -22,26 +22,6 @@ export default class Character {
             this.attack - this.defense
         } else {
             console.log("On n'attaque pas un mort !")
-        }
-    }
-
-    heal(percent : number) {
-        if(this.currentHP <= 0){
-            console.log("On ne soigne pas un mort !")
-        } else {
-            if(this.currentHP > this.currentHP + this.maxHP*(percent/100)) {
-                this.currentHP = this.maxHP
-            } else {
-                this.currentHP += this.maxHP*(percent/100)
-            }
-        }
-    }
-
-    resurection(percent : number) {
-        if(this.currentHP <= 0) {
-            this.currentHP += this.maxHP*(percent/100)
-        } else {
-            console.log("On ne ressucite pas un un personnage déjà vivant !")
         }
     }
 }
---
+++ test.ts
@@ -0,0 +1,4 @@
+import Menu from "./menu.ts";
+
+const testmenu = new Menu("test", ["attaquer","quitter"])
+console.log(testmenu.input())
