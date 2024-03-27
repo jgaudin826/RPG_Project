2d96890ebc6f6cb309ff6195d38de58a6de6bdad
cassian joly <cassian.joly@ynov.com>
2024-03-27 14:15:31 +0100

    fix a indentation in monster.ts

--- character/monster.ts
+++ character/monster.ts
@@ -3,15 +3,14 @@ import Character from "./character";
 
 export default class Monster extends Character{
     playerWithLowestHP(characters:Character[]):Character{
-    let player : Character =characters[0]
-    let lowerHP : number = characters[0].currentHp
-    characters.forEach(character => {
-        if (character.currentHp<lowerHP){
-            player=character
-            lowerHP=character.currentHp
-        }
-    });
-    return player
-}
-
+        let player : Character =characters[0]
+        let lowerHP : number = characters[0].currentHp
+        characters.forEach(character => {
+            if (character.currentHp<lowerHP){
+                player=character
+                lowerHP=character.currentHp
+            }
+        });
+        return player
+    }
 }
